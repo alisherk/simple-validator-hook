e200ae52a204ac0a30275b94dece960d60ed570e
alisher <alisherkabildjanov@yahoo.com>
2022-06-18 15:10:54 -0500

    refactored utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,30 +1,30 @@
 export const capitalize = (text: string): string =>
   text.charAt(0).toUpperCase() + text.slice(1);
 
-  type Procedure = (...args: any[]) => void;
+type Procedure = (...args: any[]) => void;
 
-  export function debounce<F extends Procedure>(
-    func: F,
-    waitMilliseconds = 50,
-    isImmediate: boolean = false
-  ): (...args: Parameters<F>) => void {
-    let timeoutId: ReturnType<typeof setTimeout> | undefined;
-  
-    return function (...args: Parameters<F>) {
-      const doLater = function () {
-        timeoutId = undefined;
-        if (!isImmediate) func(...args);
-      };
-  
-      const shouldCallNow = isImmediate && timeoutId === undefined;
-  
-      if (timeoutId) {
-        clearTimeout(timeoutId);
-      } 
-      timeoutId = setTimeout(doLater, waitMilliseconds);
-  
-      if (shouldCallNow) func(...args);
+export function debounce<F extends Procedure>(
+  func: F,
+  waitMilliseconds = 50,
+  isImmediate: boolean = false
+): (...args: Parameters<F>) => void {
+  let timeoutId: ReturnType<typeof setTimeout> | undefined;
+
+  return function (...args: Parameters<F>) {
+    const doLater = function () {
+      timeoutId = undefined;
+      if (!isImmediate) func(...args);
     };
-  }
-  
 
+    const shouldCallNow = isImmediate && timeoutId === undefined;
+
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(doLater, waitMilliseconds);
+
+    if (shouldCallNow) {
+      func(...args);
+    }
+  };
+}
